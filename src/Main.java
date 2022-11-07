43bd1db2ae7dd93c0e0db3ac02f4f90c038067a2
Niki <colt92@inbox.ru>
2022-11-08 02:31:35 +0500

    Задача к билету 4 выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
+
+    //Дан массив строк, необходимо удалить из него все дубли, оставшиеся строки объединить в одну в порядке следования в массиве.
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] names = new String[4];
+        names[0] = "James";
+        names[1] = "Nick";
+        names[2] = "Michael";
+        names[3] = "Nick";
+        System.out.println(deleteDublicates(names));
     }
+
+    public static String deleteDublicates(String[] names) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < names.length; i++) {
+            if (names[i] != null) {
+                for (int j = i + 1; j < names.length; j++) {
+                    if (names[i].equals(names[j])) {
+                        names[j] = null;
+                    }
+                }
+                sb.append(names[i] + " ");
+            }
+        }
+        return sb.toString();
+    }
+
+
+
 }
